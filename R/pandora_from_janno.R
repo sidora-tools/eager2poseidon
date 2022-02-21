95b5a492c40827e44c37d2067e83654e09582384
Thiseas Christos Lamnidis <thisseass@gmail.com>
2022-02-21 14:44:39 +0100

    Apply tidyverse styling

--- R/pandora_from_janno.R
+++ R/pandora_from_janno.R
@@ -16,42 +16,42 @@ import_pandora_data <- function(janno, credentials, trust_uncalibrated_dates) {
   ## Read janno
   input_janno <- poseidonR::read_janno(janno, to_janno = F, validate = TRUE)
   sample_ids <- input_janno %>%
-    dplyr::select(tidyselect::any_of(c('Individual_ID', 'Poseidon_ID')))
+    dplyr::select(tidyselect::any_of(c("Individual_ID", "Poseidon_ID")))
   ## Rename to Individual_ID to Poseidon_ID if the janno is from an older Poseidon version. Throw warning.
-  if (names(sample_ids) == "Individual_ID" ) {
+  if (names(sample_ids) == "Individual_ID") {
     warning("Your janno file appears to be from an older poseidon version. It will be updated to poseidon version '2.5.0'.")
     ## Change column name in imported janno and sample names
-    input_janno <- input_janno %>% dplyr::rename("Poseidon_ID"="Individual_ID")
+    input_janno <- input_janno %>% dplyr::rename("Poseidon_ID" = "Individual_ID")
     names(sample_ids) <- c("Poseidon_ID")
   }
 
-  write("Querying Pandora for individual data.", file=stderr())
+  write("Querying Pandora for individual data.", file = stderr())
   pandora_table <- get_individual_pandora_data(sample_ids, credentials)
   pandora_table <- pandora_table %>%
     dplyr::mutate(
-      Poseidon_ID=.data$individual.Full_Individual_Id,
-      Collection_ID=.data$individual.Archaeological_ID,
-      Country=.data$site.Country,
-      Location=dplyr::if_else(.data$site.Locality == "", "n/a", .data$site.Locality),
-      Longitude=.data$site.Longitude,
-      Latitude=.data$site.Latitude,
-      Date_C14_Labnr=dplyr::na_if(.data$individual.C14_Id,""),
-      Date_BC_AD_Start_pandora=dplyr::case_when(
+      Poseidon_ID = .data$individual.Full_Individual_Id,
+      Collection_ID = .data$individual.Archaeological_ID,
+      Country = .data$site.Country,
+      Location = dplyr::if_else(.data$site.Locality == "", "n/a", .data$site.Locality),
+      Longitude = .data$site.Longitude,
+      Latitude = .data$site.Latitude,
+      Date_C14_Labnr = dplyr::na_if(.data$individual.C14_Id, ""),
+      Date_BC_AD_Start_pandora = dplyr::case_when(
         ## If no C14 ID is given in pandora, don't trust the Calibrated date field, else take it as is.
         .data$Date_C14_Labnr %in% c("", NA) ~ NA_integer_,
         TRUE ~ .data$individual.C14_Calibrated_From
       ),
-      Date_BC_AD_Stop_pandora=dplyr::case_when(
+      Date_BC_AD_Stop_pandora = dplyr::case_when(
         ## If no C14 ID is given in pandora, don't trust the Calibrated date field, else take it as is.
         .data$Date_C14_Labnr %in% c("", NA) ~ NA_integer_,
         TRUE ~ .data$individual.C14_Calibrated_To
       ),
-      Date_Type_pandora=dplyr::case_when(
+      Date_Type_pandora = dplyr::case_when(
         ## If a C14 Lab number is provided, set date type to C14.
         .data$Date_C14_Labnr %in% c("", NA) ~ NA_character_,
         TRUE ~ "C14"
       ),
-      Date_Note_pandora=dplyr::case_when(
+      Date_Note_pandora = dplyr::case_when(
         ## If a C14 Lab number is provided, propagate the date info field.
         .data$Date_C14_Labnr %in% c("", NA) ~ NA_character_,
         TRUE ~ .data$individual.C14_Info
@@ -63,8 +63,8 @@ import_pandora_data <- function(janno, credentials, trust_uncalibrated_dates) {
       Date_BC_AD_Median = NA_integer_,
       Date_C14_Uncal_BP = NA_integer_,
       Date_C14_Uncal_BP_Err = NA_integer_,
-      Date_Type_quickcal=NA_character_,
-      Date_Note_quickcal=NA_character_
+      Date_Type_quickcal = NA_character_,
+      Date_Note_quickcal = NA_character_
     )
   ## If user trusts uncalibrated dates (and C14 IDs are set up correctly) for their samples,
   ##    then use that data to quickcalibrate (calibrated dates are retained if existing)
@@ -72,14 +72,14 @@ import_pandora_data <- function(janno, credentials, trust_uncalibrated_dates) {
     pandora_table <- pandora_table %>%
       dplyr::mutate(
         ## If no Lab Nr is given in pandora, the C14 data is not considered a valid entry.
-        Date_C14_Uncal_BP=dplyr::if_else(.data$Date_C14_Labnr %in% c("", NA), NA_integer_, .data$individual.C14_Uncalibrated),
-        Date_C14_Uncal_BP_Err=dplyr::if_else(.data$Date_C14_Labnr %in% c("", NA), NA_integer_, .data$individual.C14_Uncalibrated_Variation),
+        Date_C14_Uncal_BP = dplyr::if_else(.data$Date_C14_Labnr %in% c("", NA), NA_integer_, .data$individual.C14_Uncalibrated),
+        Date_C14_Uncal_BP_Err = dplyr::if_else(.data$Date_C14_Labnr %in% c("", NA), NA_integer_, .data$individual.C14_Uncalibrated_Variation),
         ## Only use calibration values if calibrated values are not in pandora
         poseidonR::quickcalibrate(
           .data$Date_C14_Uncal_BP,
           .data$Date_C14_Uncal_BP_Err,
           select_calibration_curve(.data$site.Latitude),
-          allowOutside=T
+          allowOutside = T
         ),
         Date_Type_quickcal = dplyr::if_else(.data$Date_C14_Labnr %in% c("", NA), NA_character_, "C14"),
         Date_Note_quickcal = dplyr::if_else(.data$Date_C14_Labnr %in% c("", NA), NA_character_, "quickcalibration from Date_C14_Uncal_BP and Date_C14_Uncal_BP_Err")
@@ -88,11 +88,11 @@ import_pandora_data <- function(janno, credentials, trust_uncalibrated_dates) {
   pandora_table <- pandora_table %>%
     dplyr::mutate(
       ## Prioritise pandora's calibrated dates when available.
-      Date_BC_AD_Start=dplyr::coalesce(.data$Date_BC_AD_Start_pandora,.data$Date_BC_AD_Start),
-      Date_BC_AD_Stop=dplyr::coalesce(.data$Date_BC_AD_Stop_pandora,.data$Date_BC_AD_Stop),
-      Date_Type=dplyr::coalesce(.data$Date_Type_pandora,.data$Date_Type_quickcal),
-      Date_Note=dplyr::coalesce(.data$Date_Note_pandora,.data$Date_Note_quickcal)
-      ) %>%
+      Date_BC_AD_Start = dplyr::coalesce(.data$Date_BC_AD_Start_pandora, .data$Date_BC_AD_Start),
+      Date_BC_AD_Stop = dplyr::coalesce(.data$Date_BC_AD_Stop_pandora, .data$Date_BC_AD_Stop),
+      Date_Type = dplyr::coalesce(.data$Date_Type_pandora, .data$Date_Type_quickcal),
+      Date_Note = dplyr::coalesce(.data$Date_Note_pandora, .data$Date_Note_quickcal)
+    ) %>%
     dplyr::select(
       .data$Poseidon_ID,
       .data$Collection_ID,
@@ -108,9 +108,9 @@ import_pandora_data <- function(janno, credentials, trust_uncalibrated_dates) {
       .data$Date_C14_Uncal_BP_Err,
       .data$Date_Type,
       .data$Date_Note
-      )
+    )
 
-  write("Parsing of Pandora table completed.", file=stderr())
+  write("Parsing of Pandora table completed.", file = stderr())
 
   pandora_table
 }
@@ -136,13 +136,14 @@ get_individual_pandora_data <- function(sample_ids, credentials) {
     sidora.core::get_df_list(
       c(sidora.core::make_complete_table_list(
         c("TAB_Site", "TAB_Sequencing")
-      )), con = con
+      )),
+      con = con
     )
   ) %>% sidora.core::convert_all_ids_to_values(con = con)
   DBI::dbDisconnect(con)
   pandora_table <- pandora_table %>% dplyr::filter(.data$individual.Full_Individual_Id %in% sample_ids$Poseidon_ID)
 
-  write("Information successfully pulled from Pandora.", file=stderr())
+  write("Information successfully pulled from Pandora.", file = stderr())
 
   return(pandora_table)
 }
@@ -157,17 +158,18 @@ get_individual_pandora_data <- function(sample_ids, credentials) {
 #' @export
 #'
 #' @examples
-#' select_calibration_curve(-50)  ## Northern hemisphere
-#' select_calibration_curve(50)   ## Southern Hemisphere
-#' select_calibration_curve(0)    ## 0 defaults to Northern hemisphere
-#'
+#' select_calibration_curve(-50) ## Northern hemisphere
+#' select_calibration_curve(50) ## Southern Hemisphere
+#' select_calibration_curve(0) ## 0 defaults to Northern hemisphere
 #' \dontrun{
 #' select_calibration_curve("banana")
 #' }
 select_calibration_curve <- function(Latitude) {
   ## Bchron (the backend for quickcalibrate) does not recognise NAs in the calibration curves.
   ##   Therefor, if a site has no latitude set, or it is wrongly set, stop and complain.
-  if (!is.numeric(Latitude) || is.na(Latitude)) { stop("Latitude value should be of class 'numeric', and NOT NA.") }
+  if (!is.numeric(Latitude) || is.na(Latitude)) {
+    stop("Latitude value should be of class 'numeric', and NOT NA.")
+  }
   dplyr::case_when(
     Latitude >= 0 ~ "intcal20",
     Latitude < 0 ~ "shcal20",
--- R/read_eager_results.R
+++ R/read_eager_results.R
@@ -1,4 +1,4 @@
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".")) ## Disables notes about '.' due to magrittr
+if (getRversion() >= "2.15.1") utils::globalVariables(c(".")) ## Disables notes about '.' due to magrittr
 
 #' Collect poseidon janno data from an eager TSV and its corresponding general stats table.
 #'
@@ -18,11 +18,11 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(".")) ## Disables notes 
 import_eager_results <- function(eager_tsv, general_stats_fn, prefer) {
   ## Parse TSV
   tsv_data <- read_eager_tsv(eager_tsv, prefer)
-  write("Parsing of eager input TSV completed.", file=stderr())
+  write("Parsing of eager input TSV completed.", file = stderr())
 
   ## Parse general stats table
   eager_data <- read_eager_stats_table(general_stats_fn, tsv_data)
-  write("Parsing of eager general stats table completed.", file=stderr())
+  write("Parsing of eager general stats table completed.", file = stderr())
 
   ## Collate data
   result <- tsv_data %>%
@@ -30,13 +30,13 @@ import_eager_results <- function(eager_tsv, general_stats_fn, prefer) {
     dplyr::summarise(
       # ## Keep the library IDs as they appear in the tsv, to filter for relevant lines from the general stats table
       # Libraries_Included=paste(Library_ID,collapse=";"),
-      Nr_Libraries=dplyr::n(),
-      Capture_Type=paste(.data$Seq_Type,collapse=";"),
+      Nr_Libraries = dplyr::n(),
+      Capture_Type = paste(.data$Seq_Type, collapse = ";"),
       ## UDG gets formatted here to account for 'mixed' UDG treatment when multiple libs of the same strandedness have different UDG treatment.
-      UDG=paste(.data$UDG_Treatment,collapse=";") %>% format_for_poseidon(., "UDG"),
-      Library_Built=paste(.data$Strandedness,collapse=";")
+      UDG = paste(.data$UDG_Treatment, collapse = ";") %>% format_for_poseidon(., "UDG"),
+      Library_Built = paste(.data$Strandedness, collapse = ";")
     ) %>%
-    dplyr::full_join(eager_data, by=c("Sample_Name"="Sample"))
+    dplyr::full_join(eager_data, by = c("Sample_Name" = "Sample"))
 
   result
 }
@@ -52,37 +52,41 @@ import_eager_results <- function(eager_tsv, general_stats_fn, prefer) {
 #' @export
 #' @importFrom magrittr "%>%"
 
-read_eager_tsv <- function(eager_tsv, prefer="none") {
+read_eager_tsv <- function(eager_tsv, prefer = "none") {
   ## End execution if TSV file is not found
   if (!file.exists(eager_tsv)) {
-    stop(paste0("File '",eager_tsv,"' not found."))
+    stop(paste0("File '", eager_tsv, "' not found."))
   }
-  if (! prefer %in% c("none","single","double")) {
-    stop(paste0("Invalid library strandedness preference: '",eager_tsv,"'"))
+  if (!prefer %in% c("none", "single", "double")) {
+    stop(paste0("Invalid library strandedness preference: '", eager_tsv, "'"))
   }
-  tsv_data <- readr::read_tsv(eager_tsv, col_types = 'cciiccccccc') %>%
-    dplyr::select(.data$Sample_Name, .data$Library_ID, .data$Strandedness, .data$UDG_Treatment,.data$R1,.data$R2,.data$BAM)
-    ## Filter for preferred library strandedness
-    if (prefer != "none") {
-      tsv_data <- dplyr::filter(tsv_data, .data$Strandedness == prefer)
-    }
+  tsv_data <- readr::read_tsv(eager_tsv, col_types = "cciiccccccc") %>%
+    dplyr::select(.data$Sample_Name, .data$Library_ID, .data$Strandedness, .data$UDG_Treatment, .data$R1, .data$R2, .data$BAM)
+  ## Filter for preferred library strandedness
+  if (prefer != "none") {
+    tsv_data <- dplyr::filter(tsv_data, .data$Strandedness == prefer)
+  }
   ## Separate the Library ID to pandora Library ID and Sequencing ID if the latter was used
   tsv_data <- tsv_data %>%
     dplyr::mutate(
-      Library=substr(.data$Library_ID, 1, 12),
-      Seq_Type=dplyr::case_when(
+      Library = substr(.data$Library_ID, 1, 12),
+      Seq_Type = dplyr::case_when(
         ## First try to infer from Lib_ID
         substr(.data$Library_ID, 14, 15) != "" ~ substr(.data$Library_ID, 14, 15) %>% format_for_poseidon(., "Capture_Type"),
         ## Then check R1, if provided. Assumes R1 file name starts with pandora full sequencing ID
-        !is.na(.data$R1) ~ basename(.data$R1) %>% substr(.,14,15) %>% format_for_poseidon(., "Capture_Type"),
+        !is.na(.data$R1) ~ basename(.data$R1) %>%
+          substr(., 14, 15) %>%
+          format_for_poseidon(., "Capture_Type"),
         ##  Else, check the BAM input (must be there if no R1 is provided)
-        !is.na(.data$BAM) ~ basename(.data$BAM) %>% substr(.,14,15) %>% format_for_poseidon(., "Capture_Type"),
+        !is.na(.data$BAM) ~ basename(.data$BAM) %>%
+          substr(., 14, 15) %>%
+          format_for_poseidon(., "Capture_Type"),
         ## Finally, a catch-all, just in case we have unexpected behaviours
         TRUE ~ NA_character_
       ),
-      Strandedness=format_for_poseidon(.data$Strandedness, "Library_Built")
+      Strandedness = format_for_poseidon(.data$Strandedness, "Library_Built")
     ) %>%
-    dplyr::select(-c(.data$R1,.data$R2,.data$BAM)) %>%
+    dplyr::select(-c(.data$R1, .data$R2, .data$BAM)) %>%
     dplyr::distinct()
   tsv_data
 }
@@ -97,11 +101,11 @@ read_eager_tsv <- function(eager_tsv, prefer="none") {
 #'
 format_for_poseidon <- function(x, field) {
   if (field == "Capture_Type") {
-  result <- dplyr::case_when(
-    x == "SG" ~ "Shotgun",
-    x == "TF" ~ "1240k",
-    TRUE ~ "Other"
-  )
+    result <- dplyr::case_when(
+      x == "SG" ~ "Shotgun",
+      x == "TF" ~ "1240k",
+      TRUE ~ "Other"
+    )
   }
 
   if (field == "Library_Built") {
@@ -137,72 +141,74 @@ format_for_poseidon <- function(x, field) {
 read_eager_stats_table <- function(general_stats_fn, tsv_data) {
   ## Check the file exists
   if (!file.exists(general_stats_fn)) {
-    stop(paste0("File '",general_stats_fn,"' not found."))
+    stop(paste0("File '", general_stats_fn, "' not found."))
   }
 
   ## Create a character vector of the sample and library IDs to filter out information from the general stats table
-  pull_samples = c(unique(tsv_data$Sample_Name), tsv_data$Library_ID) %>% sort()
+  pull_samples <- c(unique(tsv_data$Sample_Name), tsv_data$Library_ID) %>% sort()
 
   ## Load stats table and keep relevant columns and lines.
-  general_stats <- readr::read_tsv(general_stats_fn, na = c("", "N/A", "NA")) %>% dplyr::filter(.data$Sample %in% pull_samples) %>%
+  general_stats <- readr::read_tsv(general_stats_fn, na = c("", "N/A", "NA")) %>%
+    dplyr::filter(.data$Sample %in% pull_samples) %>%
     dplyr::select(
       .data$Sample,
-      endogenous_dna=.data$`endorSpy_mqc-generalstats-endorspy-endogenous_dna`,
-      covered_snps=.data$`snp_coverage_mqc-generalstats-snp_coverage-Covered_Snps`,
-      total_snps=.data$`snp_coverage_mqc-generalstats-snp_coverage-Total_Snps`,
-      damage_5p1=.data$`DamageProfiler_mqc-generalstats-damageprofiler-5_Prime1`,
-      x_contamination_snps=.data$`nuclear_contamination_mqc-generalstats-nuclear_contamination-Num_SNPs`,
-      x_contamination=.data$`nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_estimate`,
-      x_contamination_error=.data$`nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_SE`,
-      duplication_rate=.data$`Picard_mqc-generalstats-picard-PERCENT_DUPLICATION`,
-      filtered_mapped_reads=.data$`Samtools Flagstat (post-samtools filter)_mqc-generalstats-samtools_flagstat_post_samtools_filter-mapped_passed`
+      endogenous_dna = .data$`endorSpy_mqc-generalstats-endorspy-endogenous_dna`,
+      covered_snps = .data$`snp_coverage_mqc-generalstats-snp_coverage-Covered_Snps`,
+      total_snps = .data$`snp_coverage_mqc-generalstats-snp_coverage-Total_Snps`,
+      damage_5p1 = .data$`DamageProfiler_mqc-generalstats-damageprofiler-5_Prime1`,
+      x_contamination_snps = .data$`nuclear_contamination_mqc-generalstats-nuclear_contamination-Num_SNPs`,
+      x_contamination = .data$`nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_estimate`,
+      x_contamination_error = .data$`nuclear_contamination_mqc-generalstats-nuclear_contamination-Method1_ML_SE`,
+      duplication_rate = .data$`Picard_mqc-generalstats-picard-PERCENT_DUPLICATION`,
+      filtered_mapped_reads = .data$`Samtools Flagstat (post-samtools filter)_mqc-generalstats-samtools_flagstat_post_samtools_filter-mapped_passed`
     ) %>%
     dplyr::mutate(
-      x_contamination=dplyr::case_when(
+      x_contamination = dplyr::case_when(
         .data$x_contamination_snps == 0 ~ "n/a",
         # is.na(x_contamination) ~ "n/a",
-        TRUE ~ format(.data$x_contamination, nsmall=3, digits=0, trim=T)
+        TRUE ~ format(.data$x_contamination, nsmall = 3, digits = 0, trim = T)
       ),
-      x_contamination_error=tidyr::replace_na("n/a")
+      x_contamination_error = tidyr::replace_na("n/a")
     )
 
   ## For endogenous, only look at Shotgun libraries. If none exist return "n/a"s
   if (nrow(tsv_data %>% dplyr::filter(.data$Seq_Type == "Shotgun")) == 0) {
     samples <- unique(tsv_data$Sample_Name)
     endogenous <- rep("n/a", length(samples))
-    endogenous_per_sample <- tibble::tibble(Sample=samples, Endogenous_DNA=endogenous)
+    endogenous_per_sample <- tibble::tibble(Sample = samples, Endogenous_DNA = endogenous)
   } else {
-    endogenous_per_sample <- dplyr::right_join(general_stats, tsv_data %>% dplyr::filter(.data$Seq_Type == "Shotgun"), by=c("Sample"="Library_ID")) %>%
-      tidyr::separate(.data$Sample, sep="\\.", into=c("Sample","Library"), fill="right", extra="merge") %>%
+    endogenous_per_sample <- dplyr::right_join(general_stats, tsv_data %>% dplyr::filter(.data$Seq_Type == "Shotgun"), by = c("Sample" = "Library_ID")) %>%
+      tidyr::separate(.data$Sample, sep = "\\.", into = c("Sample", "Library"), fill = "right", extra = "merge") %>%
       dplyr::group_by(.data$Sample) %>%
       dplyr::summarise(
-        Endogenous_DNA=format(max(.data$endogenous_dna), nsmall=2, digits=0)
+        Endogenous_DNA = format(max(.data$endogenous_dna), nsmall = 2, digits = 0)
       )
   }
 
-  ##Library info for library based fields
-  contamination_per_sample <- general_stats  %>%
-    tidyr::separate(.data$Sample, sep="\\.", into=c("Sample","Library"), fill="right", extra="merge") %>%
-    dplyr::filter(!is.na(.data$Library)) %>% ## Keep only Library Entries
+  ## Library info for library based fields
+  contamination_per_sample <- general_stats %>%
+    tidyr::separate(.data$Sample, sep = "\\.", into = c("Sample", "Library"), fill = "right", extra = "merge") %>%
+    dplyr::filter(!is.na(.data$Library)) %>%
+    ## Keep only Library Entries
     dplyr::group_by(.data$Sample) %>%
     dplyr::summarise(
-      Contamination=paste(.data$x_contamination, collapse=";"),
-      Contamination_Err=paste(.data$x_contamination_error, collapse=";"),
-      Contamination_Meas=paste(rep("ANGSD", length(.data$x_contamination)), collapse=";"),
-      Contamination_Note=paste("Nr Snps:", paste(.data$x_contamination_snps, collapse=";"))
+      Contamination = paste(.data$x_contamination, collapse = ";"),
+      Contamination_Err = paste(.data$x_contamination_error, collapse = ";"),
+      Contamination_Meas = paste(rep("ANGSD", length(.data$x_contamination)), collapse = ";"),
+      Contamination_Note = paste("Nr Snps:", paste(.data$x_contamination_snps, collapse = ";"))
     )
 
   ## For the rest use everything
-  sample_stats <- general_stats  %>%
-    tidyr::separate(.data$Sample, sep="\\.", into=c("Sample","Library"), fill="right", extra="merge") %>%
+  sample_stats <- general_stats %>%
+    tidyr::separate(.data$Sample, sep = "\\.", into = c("Sample", "Library"), fill = "right", extra = "merge") %>%
     dplyr::group_by(.data$Sample) %>%
     dplyr::summarise(
-      Damage=stats::weighted.mean(stats::na.omit(.data$damage_5p1),stats::na.omit(.data$filtered_mapped_reads)),
-      Nr_SNPs=max(.data$covered_snps, na.rm=T)
+      Damage = stats::weighted.mean(stats::na.omit(.data$damage_5p1), stats::na.omit(.data$filtered_mapped_reads)),
+      Nr_SNPs = max(.data$covered_snps, na.rm = T)
     )
 
   ## Join it all together
-  result <- dplyr::full_join(sample_stats, endogenous_per_sample, by="Sample") %>%
-    dplyr::full_join(contamination_per_sample, by="Sample")
+  result <- dplyr::full_join(sample_stats, endogenous_per_sample, by = "Sample") %>%
+    dplyr::full_join(contamination_per_sample, by = "Sample")
   result
 }
